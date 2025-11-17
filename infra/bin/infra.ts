caa893a412252112f5bc8b7e6d462f25c39cdb26
Sergey Kazantsev <sergeyka@amazon.com>
2025-11-17 15:57:48 +0100

    Add source-map-support to infra
    Improved error stack traces in CDK by adding source-map-support.
    
    ---
    Prompt: Let's use the following snippet to update infra/bin/infra.ts

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { execSync } from "child_process";
 import { FrontendStack } from "../lib/stacks/frontend-stack";
